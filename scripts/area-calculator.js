1cc3db49f50a38037ba9dca18e7cfb6e2e7cee66
Jahid2121 <jahidsarkar2121@gmail.com>
2023-08-19 16:24:56 +0600

    added ellipse

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -32,3 +32,33 @@ function calculateRectangleArea(){
     const rectangleAreaSpan = document.getElementById('rectangle-area')
     rectangleAreaSpan.innerText = area
 }
+
+function calculateParallelogramArea(){
+    const base = getInputValue('parallelogram-base')
+    // console.log(base);
+    const height = getInputValue('parallelogram-height')
+    // console.log(height);
+
+    const area = base * height
+    setElementInnerText('parallelogram-area', area)
+}
+
+function calculateEllipseArea(){
+    const majorRadius = getInputValue('ellipse-major-radius')
+    const minorRadius = getInputValue('ellipse-minor-radius')
+    const area = 3.14 * majorRadius * minorRadius
+    setElementInnerText('ellipse-area', area)
+}
+
+function getInputValue(fieldId){
+    const inputField = document.getElementById(fieldId)
+    const inputValueText = inputField.value;
+    const value = parseFloat(inputValueText)
+    return value
+}
+
+function setElementInnerText(elementFieldId, area){
+    const element = document.getElementById(elementFieldId)
+    element.innerText = area
+}
+    
